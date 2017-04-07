4bacdb799a40eb0f1f314f83c8b9fd6fae500b68
Ian Cowley <spuriousbugfix@hotmail.com>
2017-04-07 16:53:40 +0100

    commented it

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string a = "lolsdlsdflkjsdlfk";
-			Console.WriteLine(a);
+			Console.WriteLine(a); // <-- I did this
         }
     }
 }
